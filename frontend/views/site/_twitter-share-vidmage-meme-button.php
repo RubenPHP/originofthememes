b27206e3dc4853bf3a2b5a93d0ceacc08dcf3c54
RubenDjOn <ruben@indahouser.com>
2015-07-27 20:42:47 +0000

    - Deleted old templates

--- frontend/views/site/_twitter-share-vidmage-meme-button.php
+++
@@ -1,6 +0,0 @@
-<?php
-use yii\helpers\Html;
-?>
-<a class="twitter-share-button"
-  href="https://twitter.com/intent/tweet?text=<?= Html::encode($text) ?>&url=<?= $vidmageMeme->fullSiteUrl ?>">
-Tweet</a>
