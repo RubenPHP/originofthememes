4cc1db196501710ce2a836afd11bde012f0cd895
RubenDjOn <ruben@indahouser.com>
2015-08-07 21:49:15 +0000

    - Added clicky tracking code

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -100,6 +100,20 @@ AppAsset::register($this);
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
 
+    <a title="Web Analytics" href="http://clicky.com/100866915"><img alt="Web Analytics" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
+    <script type="text/javascript">
+    var clicky_site_ids = clicky_site_ids || [];
+    clicky_site_ids.push(100866915);
+    (function() {
+      var s = document.createElement('script');
+      s.type = 'text/javascript';
+      s.async = true;
+      s.src = '//static.getclicky.com/js';
+      ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
+    })();
+    </script>
+    <noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/100866915ns.gif" /></p></noscript>
+
     <?php $this->endBody() ?>
 </body>
 </html>
